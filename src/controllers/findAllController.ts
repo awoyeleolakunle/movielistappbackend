2da7635b826d8f35e5d311a8cc706515a521b433
Awoyele Olakunle <awoyeleolakunle@gmail.com>
2024-01-20 16:48:02 +0100

    Updated commit

---
+++ src/controllers/findAllController.ts
@@ -0,0 +1,25 @@
+
+import Movie, {MovieModel } from "../models/movieModel";
+
+import { Response, Request } from "express";
+
+import { ErrorMessage, HttpStatus } from '../constant';
+
+
+
+
+
+
+export const findAllMovies = async (req : Request, res: Response)=>{
+
+    try{
+    const allMovies : MovieModel[] = await Movie.find();
+
+    res.status(HttpStatus.OK).json(allMovies);
+    }
+    catch(error){
+
+        console.log("An error occurred: ", error);
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(ErrorMessage.INTERNAL_SERVER_ERROR_MESSAGE);
+    }
+}
--- src/routes/movieRoutes.ts
+++ src/routes/movieRoutes.ts
@@ -3,6 +3,7 @@
 import { Router } from 'express';
 import { createMovie } from '../controllers/movieController';
 import { findMovieById } from '../controllers/findMovieController';
+import { findAllMovies } from '../controllers/findAllController';
 
 
 
@@ -12,5 +13,6 @@ const router = Router();
 router.post('/movieCreation', createMovie);
 
 router.get('/findMovie', findMovieById);
+router.get('/allMovie', findAllMovies);
  
 export default router;
